4f51daa69e1c3e3b394152c08bad5d4a45f31966
Krupali Italiya <63084331+Krupali0609@users.noreply.github.com>
2021-03-09 18:58:08 -0500

    Update Test_Testing.py

--- Test_Testing.py
+++ Test_Testing.py
@@ -6,7 +6,7 @@ class TestgetRepo(unittest.TestCase):
         expected = ['User: Krupali0609',
                     'Repository Name: HelloWorld Number of commits in Repository: 2',
                     'Repository Name: Software-Quality-Assurance-and-Testing-567 Number of commits in Repository: 2',
-                    'Repository Name: SSW567_HW04 Number of commits in Repository: 5',
+                    'Repository Name: SSW567_HW04 Number of commits in Repository: 9',
                     'Repository Name: Student-Repository Number of commits in Repository: 23',
                     'Repository Name: Triangle567 Number of commits in Repository: 11']
 
