5d02ea49691bb82be67e6450ffbf712b5787352e
Krupali Italiya <63084331+Krupali0609@users.noreply.github.com>
2021-03-09 18:57:36 -0500

    Add files via upload

---
+++ Test_Testing.py
@@ -0,0 +1,17 @@
+import unittest
+from Testing import get_repository_details
+
+class TestgetRepo(unittest.TestCase):
+    def test_repo(self):
+        expected = ['User: Krupali0609',
+                    'Repository Name: HelloWorld Number of commits in Repository: 2',
+                    'Repository Name: Software-Quality-Assurance-and-Testing-567 Number of commits in Repository: 2',
+                    'Repository Name: SSW567_HW04 Number of commits in Repository: 5',
+                    'Repository Name: Student-Repository Number of commits in Repository: 23',
+                    'Repository Name: Triangle567 Number of commits in Repository: 11']
+
+        self.assertEqual(get_repository_details('Krupali0609'), expected)
+
+"""excution starts from here"""
+if __name__ == '__main__':   
+    unittest.main()
