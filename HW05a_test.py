752dacf6b1f3f12b7a4b0df3eea0f12b8a98ff2e
Krupali Italiya <63084331+Krupali0609@users.noreply.github.com>
2021-03-16 16:15:39 -0400

    Add files via upload

---
+++ HW05a_test.py
@@ -0,0 +1,38 @@
+"""Test cases for fetching repository
+Author:Krupali Italiya
+"""
+
+import unittest
+import json
+from unittest import mock
+from HW05a import get_repo_details
+
+
+def get_url(url):
+    if url == "https://api.github.com/users/themalavshastri/repos":
+        return fetch_repo('rep.json')
+
+class content:
+    content = ''
+    
+def fetch_repo(loc):
+    data = content()
+    with open(loc,'r') as f:
+        data.content = json.loads(f)
+    return data.content
+
+
+class TestRepo(unittest.TestCase):
+    """Test the Repos"""
+
+    @mock.patch('requests.get')
+    def test_repos(self,mocked):
+        """Testing the repository and commits"""
+        mocked.side_effect = get_url
+        reposit = get_repo_details('themalavshastri')
+        self.assertEqual(reposit,"unable to fetch user's repos")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
