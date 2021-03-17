84ff4063d56674ddaec249a3e3c50256599d55f2
Krupali Italiya <63084331+Krupali0609@users.noreply.github.com>
2021-03-16 21:02:58 -0400

    Update Test_Testing.py

--- Test_Testing.py
+++ Test_Testing.py
@@ -4,12 +4,13 @@ from Testing import get_repository_details
 class TestgetRepo(unittest.TestCase):
     def test_repo(self):
         expected = ['User: Krupali0609',
-                    'Repository Name: HelloWorld Number of commits in Repository: 2',
-                    'Repository Name: Software-Quality-Assurance-and-Testing-567 Number of commits in Repository: 2',
-                    'Repository Name: SSW567_HW04 Number of commits in Repository: 14',
-                    'Repository Name: SSW567_HW04-1 Number of commits in Repository: 14',
-                    'Repository Name: Student-Repository Number of commits in Repository: 23',
-                    'Repository Name: Triangle567 Number of commits in Repository: 12']
+'Repository Name: HelloWorld Number of commits in Repository: 2',
+'Repository Name: Software-Quality-Assurance-and-Testing-567 Number of commits in Repository: 2',
+'Repository Name: SSW567-Triangle Number of commits in Repository: 9',
+'Repository Name: SSW567_HW04 Number of commits in Repository: 15',
+'Repository Name: SSW567_HW04-1 Number of commits in Repository: 13',
+'Repository Name: Student-Repository Number of commits in Repository: 23',
+'Repository Name: Triangle567 Number of commits in Repository: 12']
 
         self.assertEqual(get_repository_details('Krupali0609'), expected)
 
