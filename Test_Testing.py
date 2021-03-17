e5a77909e2b193793afbfe14cc4dab7f7b82b3e3
Krupali Italiya <63084331+Krupali0609@users.noreply.github.com>
2021-03-16 21:05:21 -0400

    Update Test_Testing.py

--- Test_Testing.py
+++ Test_Testing.py
@@ -7,10 +7,10 @@ class TestgetRepo(unittest.TestCase):
 'Repository Name: HelloWorld Number of commits in Repository: 2',
 'Repository Name: Software-Quality-Assurance-and-Testing-567 Number of commits in Repository: 2',
 'Repository Name: SSW567-Triangle Number of commits in Repository: 9',
-'Repository Name: SSW567_HW04 Number of commits in Repository: 15',
+'Repository Name: SSW567_HW04 Number of commits in Repository: 16',
 'Repository Name: SSW567_HW04-1 Number of commits in Repository: 13',
 'Repository Name: Student-Repository Number of commits in Repository: 23',
-'Repository Name: Triangle567 Number of commits in Repository: 12']
+'Repository Name: Triangle567 Number of commits in Repository: 11']
 
         self.assertEqual(get_repository_details('Krupali0609'), expected)
 
